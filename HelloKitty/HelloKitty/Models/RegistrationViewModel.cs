a85620372375ce26dd9cdeb5eb9ccaaedd47bdc3
Lili <charlyrsmail@gmail.com>
2021-02-25 00:44:19 +0300

    minus glaza

---
+++ HelloKitty/HelloKitty/Models/RegistrationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+
+namespace HelloKitty.Models{
+    public class RegistrationViewModel{
+        public string Email{ get; set; }
+        public string Password{ get; set;}
+        public string Name{ get; set; }
+        
+    }
+
+    
+}
